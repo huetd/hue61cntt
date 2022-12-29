e9a597d6ecc729ce05436b6326012bec67c3ac7a
huetd <119486629+huetd@users.noreply.github.com>
2022-12-30 00:21:51 +0700

    Add files via upload

---
+++ ChuongTrinh4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	printf("Enter the desired Choice:	1\n");
+
+	printf("\nd8888b.  .d88b.   .d88b.  db   dD      d88888b db      d888888b  d888b  db   db d888888b\n");
+	printf("88  `8D .8P  Y8. .8P  Y8. 88 ,8P'      88'     88        `88'   88' Y8b 88   88 `~~88~~'\n");
+	printf("88oooY' 88    88 88    88 88,8P        88ooo   88         88    88      88ooo88    88   \n");
+	printf("88~~~b. 88    88 88    88 88`8b        88~~~   88         88    88  ooo 88~~~88    88   \n");
+	printf("88   8D `8b  d8' `8b  d8' 88 `88.      88      88booo.   .88.   88. ~8~ 88   88    88   \n");
+	printf("Y8888P'  `Y88P'   `Y88P'  YP   YD      YP      Y88888P Y888888P  Y888P  YP   YP    YP   \n");
+}
